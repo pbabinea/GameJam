8a9fd47d2f1e01e57217842b951a7a6dd7818654
Phoebe Babineau <46791979+pbabinea@users.noreply.github.com>
2020-09-23 17:54:38 -0500

    Score increases with time
    Also moved scripts to a script folder and set backgrounds to black

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float elapsed = 0f; //used for time-based score
+    public int score = 0;
+    public TMPro.TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //add 100 points every second
+        elapsed += Time.deltaTime;
+        if (elapsed >= 1f)
+        {
+            score += 100;
+            elapsed = 0;
+        }
+
+        scoreText.text = "Score:\n " + score;
+    }
+}
