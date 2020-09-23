3e9aa7d3d33b5d3479be2770f42054b3af839170
Phoebe Babineau <46791979+pbabinea@users.noreply.github.com>
2020-09-23 17:13:30 -0500

    Added simple menus (start, pause, end screen)

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //begin game for main menu button
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    //retry game from end screen (same as BeginGame now but may need updates later)
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    //return to main menu (resets the game)
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    //unpause from pause menu
+    public void Unpause()
+    {
+        Manager.instance.UnpauseGame();
+    }
+}
---
+++ Assets/Manager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager instance;
+    private bool isPaused = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Time.timeScale = 1;
+        if (instance)
+            Destroy(gameObject);
+        else
+            instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //use esc to pause/unpause
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                UnpauseGame();
+                isPaused = false;
+            }
+        }
+    }
+
+    //pause game and load pause menu
+    void PauseGame()
+    {
+        Debug.Log("pause");
+        Time.timeScale = 0;
+        isPaused = true;
+        SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+    }
+    //unpause and return to game
+    public void UnpauseGame()
+    {
+        Debug.Log("unpause");
+        SceneManager.UnloadSceneAsync("PauseMenu");
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+}
