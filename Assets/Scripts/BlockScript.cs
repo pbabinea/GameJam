bef7abaf53d3f37cf6cc259a89e091b753cde2d1
Leekola Annairie <rlaurits@trinity.edu>
2020-09-27 15:59:00 -0500

    blocks spawn wooo

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    public GameObject antag;
+    public float speed;
+    public float initPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
+        if (transform.position.x < -11)
+            Destroy(this.gameObject);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ("Player".Equals(collision.gameObject.tag))
+        {
+            UnityEngine.Debug.Log("collision");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    public static int FPS = 50;
+    private int beatLength;
+    private int frame;
+    private int index = 0;
+    private List<int> lanes = new List<int>{0,4,-4,-2,2,0,2,-2,4,0,-4};
+    public GameObject blockPrefab;
+    public GameObject antag;
+    public float xPos;
+    public float spawnRate;
+    public float spawnDelay;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        beatLength = FPS * 60 / antag.GetComponent<BeatScript>().BPM; //get number of frames per beat
+        frame = beatLength / 2;
+
+        /*for(int i = 0; i < lanes.Count; i++)
+        {
+            float sample = Mathf.PerlinNoise(xPos, 0) * 4;
+            sample = Mathf.Round(sample);
+            lanes[i] = sample;
+        }*/
+        //I wanted to try to randomly generate some of the positions, but the code has other ideas, i guess :/
+    }
+        
+
+    private void FixedUpdate()
+    {
+        frame++;
+        if (frame >= beatLength)
+        {
+            //Instantiate(blockPrefab, new Vector3(xPos, 0, 0), Quaternion.identity);
+            Debug.Log("index" + index);
+            Spawn(lanes[index]);
+            index++;
+            //Spawn(lanes[index]);
+            frame = 0;
+            if (index == lanes.Count)
+            {
+                index = 0;
+            }
+        }
+        
+    }
+
+    //void Spawn()
+    void Spawn(float lane)
+    {
+        //GameObject newBlock = Instantiate(blockPrefab, spawnPoint);
+        GameObject newBlock = Instantiate(blockPrefab, new Vector3(xPos, lane, 0), Quaternion.identity);       
+    }
+    //void SpawnTwoAdjacent
+    //void SpawnSpeedy
+    //void SpawnLongBoy
+    //void SpawnDouble
+}
