c61d356b77e1d42e29e589ad01b3d56d1eb1c96a
Phoebe Babineau <46791979+pbabinea@users.noreply.github.com>
2020-09-27 17:22:43 -0500

    game over method

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI scoreText;
+    public TMPro.TextMeshProUGUI highscoreText;
+    // Update is called once per frame
+    void Start()
+    {
+        int finalScore = PlayerPrefs.GetInt("finalScore");
+        scoreText.text = "FINAL SCORE:\n " + finalScore;
+        int highscore = PlayerPrefs.GetInt("highscore");
+        if (finalScore > highscore)
+            PlayerPrefs.SetInt("highscore", finalScore);
+        highscoreText.text = "HIGH SCORE:\n " + highscore;
+    }
+}
